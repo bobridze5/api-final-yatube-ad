d21ff4df63b4711b254e0b691dd0282c55f00655
Alsaev Dmitry <dima101203@gmail.com>
2026-02-10 11:24:50 +0700

    feat: add Follow & Group models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,6 +4,26 @@ from django.db import models
 User = get_user_model()
 
 
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'following'], name='unique_follow')
+        ]
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
@@ -11,6 +31,8 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL, related_name='posts', null=True, blank=True)
 
     def __str__(self):
         return self.text
